1cd4e816f433b0468113f860f162777dae31de95
GlaysonL <glayson.leo@gmail.com>
2023-06-18 01:20:28 -0300

    Implementados as buscas simples e agrupadas.

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,20 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System;
-
 
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
-        public string Name{ get; set; }
-
+        public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
-
         }
 
         public Department(int id, string name)
@@ -33,6 +30,4 @@ namespace SalesWebMvc.Models
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
     }
-
-   
 }
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -19,33 +19,34 @@ namespace SalesWebMvc.Models
         public string Email { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
-        [Display(Name = "Birth Date" )]
+        [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")] // alterar para padrão de dia, mes e ano.
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
-
 
         [Required(ErrorMessage = "{0} required")]
         [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
-        [DisplayFormat(DataFormatString ="{0:F2}")] // Duas casas decimais no salario.
-        public double BaseSalary  { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
-
-        public Seller() { }
+        public Seller()
+        {
+        }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
-            this.Email = email;
-            this.BirthDate = birthDate;
-            this.BaseSalary = baseSalary;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
             Department = department;
-            
         }
 
         public void AddSales(SalesRecord sr)
@@ -62,6 +63,5 @@ namespace SalesWebMvc.Models
         {
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
-
     }
 }
